0fe2176d3311a572d80ed1b04f93a4510224c8e6
nathancao <ncao2023@chadwickschool.org>
2022-12-01 21:25:31 -0800

    add: test file with functional map centered at Wick, going to try and create my own tileset

--- wick-gps/src/App.js
+++ wick-gps/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/test'
 
 function App() {
   return (
     <div className = "outer">
-      
+      <Test />
     </div>
   );
 }
---
+++ wick-gps/src/components/test.js
@@ -0,0 +1,30 @@
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import { Marker, Popup} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css' //this one line literally imports the code in the <link> and the <script> tags. so that's how you're supposed to do it?
+// check out maptiler; it would be fun to create my own tileset
+
+const center = [33.77690600305485, -118.36135411842548]
+
+function Test() {
+    return(
+        <MapContainer 
+            center={center} 
+            zoom={13} 
+            style={{width: "100vw", height: "100vh"}}
+            scrollWheelZoom={false}>
+        <TileLayer
+             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+        </MapContainer>
+    )
+}
+
+export default Test;
